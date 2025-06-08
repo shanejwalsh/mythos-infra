55635e951c260978f98ef1418da3405e0b302216
Shane Walsh <shanewalsh@hey.com>
2025-06-08 13:23:18 +0100

    adds route 53 and dns to domain

--- lib/mythos-infra-stack.ts
+++ lib/mythos-infra-stack.ts
@@ -11,6 +11,9 @@ import * as iam from "aws-cdk-lib/aws-iam";
 // import * as secretsmanager from "aws-cdk-lib/aws-secretsmanager";
 // import * as ecr from "aws-cdk-lib/aws-ecr";
 import * as cloudfront from "aws-cdk-lib/aws-cloudfront";
+import * as targets from "aws-cdk-lib/aws-route53-targets";
+
+import * as acm from "aws-cdk-lib/aws-certificatemanager";
 
 import * as origins from "aws-cdk-lib/aws-cloudfront-origins";
 
@@ -24,8 +27,13 @@ export class MythosInfraStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const hostedZone = new route53.HostedZone(this, "MythosHostedZone", {
-      zoneName: "mythosapp.io",
+    const zone = route53.HostedZone.fromLookup(this, "MythosZone", {
+      domainName: "mythosapp.io",
+    });
+
+    const cert = new acm.Certificate(this, "MythosCert", {
+      domainName: "mythosapp.io",
+      validation: acm.CertificateValidation.fromDns(zone),
     });
 
     const vpc = new ec2.Vpc(this, "MythosVpc", {
@@ -123,6 +131,8 @@ export class MythosInfraStack extends cdk.Stack {
       this,
       "MythosDistribution",
       {
+        certificate: cert,
+        domainNames: ["mythosapp.io"],
         defaultBehavior: {
           origin:
             origins.S3BucketOrigin.withOriginAccessControl(frontendBucket),
@@ -146,6 +156,14 @@ export class MythosInfraStack extends cdk.Stack {
         priceClass: cloudfront.PriceClass.PRICE_CLASS_100,
       },
     );
+
+    new route53.ARecord(this, "RootAlias", {
+      zone,
+      recordName: "mythosapp.io",
+      target: route53.RecordTarget.fromAlias(
+        new targets.CloudfrontTarget(distribution),
+      ),
+    });
 
     githubUser.addToPolicy(
       new iam.PolicyStatement({
